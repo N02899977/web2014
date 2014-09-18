c5f8571907a21635671664bf317cdb80e489df02
Heidi Fritz <fritz.heidi@gmail.com>
2014-09-18 16:00:47 -0400

    commit

--- inc/nav.php
+++ inc/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
     <div class="container">
-        <div class="dropdown">
+        <button class="dropdown">
             <a data-toggle="dropdown" href="#">Profile</a>
             <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
                 <li href="">Settings</li>
