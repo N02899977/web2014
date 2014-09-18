54499e905b15ea00379c0b6f24adb75adccb714e
Heidi Fritz <Fritz.heidi@gmail.com>
2014-09-18 00:37:09 -0400

    Update nav.php

---
+++ inc/nav.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <div class="container">
+        <div class="dropdown">
+            <a data-toggle="dropdown" href="#">Profile</a>
+            <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+                <li href="">Settings</li>
+            </ul>        
+        </div>
+    </div>
+</nav>
