c36ee723f0e5f4275694e6701462fb88d5099193
Heidi Fritz <fritz.heidi@gmail.com>
2014-09-18 16:09:52 -0400

    nav

--- inc/nav.php
+++ inc/nav.php
@@ -5,6 +5,12 @@
             <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
                 <li href="">Settings</li>
             </ul>        
-        </div>
+        </button>
+        <ul class="nav nav-tabs" role="tablist">
+		  <li class="active"><a href="#home" role="tab" data-toggle="tab">Home</a></li>
+		  <li><a href="#profile" role="tab" data-toggle="tab">Profile</a></li>
+		  <li><a href="#messages" role="tab" data-toggle="tab">Messages</a></li>
+		  <li><a href="#settings" role="tab" data-toggle="tab">Settings</a></li>
+		</ul>
     </div>
 </nav>
