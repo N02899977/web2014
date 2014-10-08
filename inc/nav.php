6ca16695f515b305460e606f3af62750bb0d82a4
Heidi Fritz <fritz.heidi@gmail.com>
2014-10-07 21:23:26 -0400

    added carousel

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,8 @@
 <nav class="navbar">
     <div class="container">
         <ul class="nav nav-tabs" role="tablist">
-		  <li class="active"><a href="#home" role="tab" data-toggle="tab">Home</a></li>
-		  <li><a href="#profile" role="tab" data-toggle="tab">Profile</a></li>
+		  <li class="active"><a href="index.html" role="tab" data-toggle="tab">Home</a></li>
+		  <li><a href="profile.html" role="tab" data-toggle="tab">Profile</a></li>
 		  <li><a href="#fitness" role="tab" data-toggle="tab">Fitness</a></li>
 		  <li><a href="#diet" role="tab" data-toggle="tab">Diet</a></li>
 		  <li><a href="#goals" role="tab" data-toggle="tab">Goals</a></li>
