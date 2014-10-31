6c95915c53f5fae6a20f435153f71e0db002fb49
Heidi Fritz <fritz.heidi@gmail.com>
2014-10-30 21:54:06 -0400

    new pages created

--- inc/nav.php
+++ inc/nav.php
@@ -8,20 +8,20 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Home</a>
+      <a class="navbar-brand" href="index.html">Home</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="profile"><a href="profile.html">Profile</a></li>
-        <li class="fitness"><a href="#">Fitness</a></li>
-        <li class="diet"><a href="#">Diet</a></li>
-        <li class="goals"><a href="#">Goals</a></li>
+        <li class="fitness"><a href="fitness.html">Fitness</a></li>
+        <li class="diet"><a href="diet.html">Diet</a></li>
+        <li class="goals"><a href="goals.html">Goals</a></li>
       </ul>
       <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
+          <input type="text" class="form-control" placeholder="Search...">
         </div>
         <button type="submit" class="btn btn-default">Search</button>
       </form>
